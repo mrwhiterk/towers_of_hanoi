202909f1bf0dcaa984a4b95a3d9aa311d7ec249d
Ryan White <mrwhiterk@gmail.com>
2019-02-07 12:46:56 -0500

    added display current block feature

--- app.js
+++ app.js
@@ -4,7 +4,28 @@ const tower2 = document.querySelector("#tower2");
 const tower3 = document.querySelector("#tower3");
 const boxWidth = 120;
 let currentPiece;
-const numBlocks = 5;
+const numBlocks = 3;
+
+const getLastItem = list => list.childNodes[(list.childNodes.length - 1)];
+
+const display = () => {
+
+
+  let status = document.querySelector("#status");
+
+  while (status.firstChild) {
+    status.removeChild(status.firstChild);
+  }
+  if (currentPiece) {
+
+    let elemClone = currentPiece.cloneNode(true);
+    status.appendChild(elemClone);
+  } else {
+    status.innerHTML = "-- select block --";
+  }
+
+
+}
 
 // create 3 blocks
 for (let i = numBlocks; i > 0; i--) {
@@ -16,11 +37,11 @@ for (let i = numBlocks; i > 0; i--) {
   tower1.appendChild(block);
 }
 
-const getLastItem = list => list.childNodes[(list.childNodes.length - 1)];
 
 function createMovement(tower) {
   if (!currentPiece) {
     currentPiece = getLastItem(tower);
+    display();
   } else if (currentPiece && tower.childNodes.length === 0) {
 
     tower.appendChild(currentPiece);
@@ -40,3 +61,5 @@ function createMovement(tower) {
 tower1.addEventListener('click', (evt) => createMovement(evt.target));
 tower2.addEventListener('click', (evt) => createMovement(evt.target));
 tower3.addEventListener('click', (evt) => createMovement(evt.target));
+
+display();

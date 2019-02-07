4d7bdb19c2dd6b689e56f00ffcaf4007da80ef42
Ryan White <mrwhiterk@gmail.com>
2019-02-07 14:20:09 -0500

    added win message

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const grid = document.querySelector(".grid");
 const tower1 = document.querySelector("#tower1");
 const tower2 = document.querySelector("#tower2");
 const tower3 = document.querySelector("#tower3");
+let status = document.querySelector("#status");
 const boxWidth = 120;
 let currentPiece;
 const numBlocks = 3;
@@ -9,22 +10,15 @@ const numBlocks = 3;
 const getLastItem = list => list.childNodes[(list.childNodes.length - 1)];
 
 const display = () => {
-
-
-  let status = document.querySelector("#status");
-
   while (status.firstChild) {
     status.removeChild(status.firstChild);
   }
   if (currentPiece) {
-
     let elemClone = currentPiece.cloneNode(true);
     status.appendChild(elemClone);
   } else {
     status.innerHTML = "-- select block --";
   }
-
-
 }
 
 // create 3 blocks
@@ -33,6 +27,10 @@ for (let i = numBlocks; i > 0; i--) {
   block.className = "block";
   block.style.width = (boxWidth * (i / 3) + "px");
   tower1.appendChild(block);
+}
+
+function didPlayerWin() {
+  return tower1.childNodes.length === 0 && tower2.childNodes.length === 0;
 }
 
 
@@ -54,6 +52,10 @@ function createMovement(tower) {
     }
   }
   display();
+
+  if (didPlayerWin()) {
+    alert("Congrats, you win!");
+  }
 }
 
 tower1.addEventListener('click', (evt) => createMovement(evt.target));

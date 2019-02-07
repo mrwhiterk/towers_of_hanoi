9e46e7d6852b3d95ffb0ad4e6176370bd667bfcd
Ryan White <mrwhiterk@gmail.com>
2019-02-07 11:12:30 -0500

    added basic functionality to meet minimum requirements

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const grid = document.querySelector(".grid");
 const tower1 = document.querySelector("#tower1");
 const tower2 = document.querySelector("#tower2");
 const tower3 = document.querySelector("#tower3");
-
-let boxWidth = 192;
-
+const boxWidth = 192;
 let currentPiece;
 
 // create 3 blocks
@@ -19,6 +17,70 @@ for (let i = 0; i < 3; i++) {
   } else {
     block.style.width = (boxWidth * 0.50) + "px";
   }
-
   tower1.appendChild(block);
 }
+
+function getLastItem(list) {
+  return list.childNodes[(list.childNodes.length - 1)]
+}
+
+tower1.addEventListener('click', function (evt) {
+
+  if (!currentPiece) {
+    currentPiece = getLastItem(evt.target);
+  } else if (currentPiece && evt.target.childNodes.length == 0) {
+
+    evt.target.appendChild(currentPiece);
+    currentPiece = null;
+  } else if (currentPiece) {
+
+    var currentPieceWidth = parseInt(currentPiece.style.width);
+    var topPieceWidth = parseInt(getLastItem(evt.target).style.width);
+    if (currentPieceWidth <= topPieceWidth) {
+      evt.target.appendChild(currentPiece);
+      currentPiece = null;
+    }
+  }
+  console.log('current piece', currentPiece);
+
+})
+
+tower2.addEventListener('click', function (evt) {
+
+  if (!currentPiece) {
+    currentPiece = evt.target.childNodes[(evt.target.childNodes.length - 1)];
+  } else if (currentPiece && evt.target.childNodes.length == 0) {
+
+    evt.target.appendChild(currentPiece);
+    currentPiece = null;
+  } else if (currentPiece) {
+
+    var currentPieceWidth = parseInt(currentPiece.style.width);
+    var topPieceWidth = parseInt(getLastItem(evt.target).style.width);
+    if (currentPieceWidth <= topPieceWidth) {
+      evt.target.appendChild(currentPiece);
+      currentPiece = null;
+    }
+  }
+  console.log('current piece', currentPiece);
+})
+
+tower3.addEventListener('click', function (evt) {
+
+  if (!currentPiece) {
+    currentPiece = evt.target.childNodes[(evt.target.childNodes.length - 1)];
+  } else if (currentPiece && evt.target.childNodes.length == 0) {
+
+    evt.target.appendChild(currentPiece);
+    currentPiece = null;
+  } else if (currentPiece) {
+
+    var currentPieceWidth = parseInt(currentPiece.style.width);
+    var topPieceWidth = parseInt(getLastItem(evt.target).style.width);
+    if (currentPieceWidth <= topPieceWidth) {
+      evt.target.appendChild(currentPiece);
+      currentPiece = null;
+    }
+  }
+  console.log('current piece', currentPiece);
+})
